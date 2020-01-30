da0e22aaa0b89fce64505b143ce9b3861d427971
tjeandot <teofilo.jeandot@gmail.com>
2020-01-30 11:39:10 +0100

    postgres project

---
+++ models/post.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+  }, {});
+  
+  Post.associate = (models) => {
+    Post.belongsTo(models.Author)
+  }
+  
+  return Post;
+};
